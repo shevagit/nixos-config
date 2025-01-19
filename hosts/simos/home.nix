420262edf3bbe73ca505d2aeb6755d501866e68f
sheva <shevaneo@gmail.com>
2025-01-19 15:45:27 +0200

    change home.nix

--- hosts/simos/home.nix
+++ hosts/simos/home.nix
@@ -1,15 +1,13 @@
+{ config, pkgs, ... }:
+
 {
+  # Set the username and home directory
   home.username = "sheva";
   home.homeDirectory = "/home/sheva";
 
+  # Enable Home Manager
   programs.home-manager.enable = true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+  # Set the state version
   home.stateVersion = "24.11"; # Please read the comment before changing.
 }
