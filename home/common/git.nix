3b36451a02048a480fbaf28506401e3c56392c57
sheva <shevaneo@gmail.com>
2025-01-22 17:43:35 +0200

    use separate git configs and include them

--- home/common/git.nix
+++ home/common/git.nix
@@ -1,8 +1,6 @@
 {
   programs.git = {
     enable = true;
-    userName = "andreas sheva";
-    userEmail = "shevaneo@gmail.com";
     aliases = {
       retag = "!f() { git tag -f -a \"$1\" -m \"$1\" && git push origin \"$1\" -f; }; f";
       deltag = "!f() { git tag -d \"$1\" && git push --delete origin \"$1\"; }; f";
@@ -11,6 +9,41 @@
       init = {
         defaultBranch = "main";
       };
+      gpg = {
+        format = "ssh";
+      };
+      user = {
+        signingkey = "~/.ssh/nixsimos-github.pub";
+      };
+      "includeIf.gitdir:/home/sheva/githubdir/" = {
+        path = "~/.gitconfig-github";
+      };
+      "includeIf.gitdir:/home/sheva/learnworlds/gitlabdir/" = {
+        path = "~/.gitconfig-gitlab";
+      };
+    };
+  };
+
+  # create separate configuration files for each use
+  home.file = {
+    ".gitconfig-github" = {
+      text = ''
+        [user]
+            name = sheva
+            email = shevaneo@gmail.com
+        [core]
+            sshCommand = ssh -i ~/.ssh/nixsimos-github.pub
+      '';
+    };
+
+    ".gitconfig-gitlab" = {
+      text = ''
+        [user]
+            name = Andreas Sevastos
+            email = sheva@learnworlds.com
+        [core]
+            sshCommand = ssh -i ~/.ssh/ed25519_gitlab.pub
+      '';
     };
   };
 }
