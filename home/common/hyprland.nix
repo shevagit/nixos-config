2d1b8cf3fb96e473b7c05e25df08b4be1826e16b
sheva <shevaneo@gmail.com>
2025-01-25 18:55:34 +0200

    modify format for hyprland config

--- home/common/hyprland.nix
+++ home/common/hyprland.nix
@@ -1,25 +1,31 @@
 { pkgs, lib, ... }: 
                                                                           
 {
-  programs.kitty = {
+  programs.terminator = {
     enable = true;
   };
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       "$mod" = "SUPER";
-      monitor = [
-        "DP-3,2560x1440@164,0x0"
-        "DP-2,1080x1920@60,2560x0,transform,90"
-      ];
+    monitor = [
+      "DP-3,2560x1440@164,0x0,scale=1,transform=none"
+      "DP-2,1080x1920@60,2560x0,scale=1,transform=90"
+    ];
+
 
       # exec-once = "swaybg -o DP-3 -i ~/wallpapers/landscape.jpg -m fill";
       # exec-once = "swaybg -o DP-2 -i ~/wallpapers/portrait.jpg -m fill";
       bind = [
         "$mod+Return exec terminator"
         "$mod+d exec wofi --show drun"
+        "$mod+Shift+r exec hyprctl reload"
+        "$mod+Shift+e exec hyprctl dispatch exit 0"
       ];
-      env = "WLR_NO_HARDWARE_CURSORS,1";
+      env = [
+        "WLR_NO_HARDWARE_CURSORS=1"
+      ];
+
       sensitivity = "1.0";
     };
   };
