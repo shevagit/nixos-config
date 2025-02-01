06bc1910bd592df732855c9bcd314cd7f85d3e82
sheva <shevaneo@gmail.com>
2025-02-02 00:24:21 +0200

    add bindings for windows and workspaces

--- home/common/hyprland.nix
+++ home/common/hyprland.nix
@@ -34,16 +34,66 @@
 
         # Bind SUPER+p to the wofi power menu script.
         "$mod, p, exec, ~/.config/hyprland/scripts/wofi-power-menu.sh"
+        # logout; to be removed
         "$mod SHIFT, e, exec, hyprctl dispatch exit 0"
+        # close active window
+        "$mod, q, killactive,"
 
+        "CONTROL, Space, togglefloating,"
+        
+        "$mod, F, fullscreen,"
 
+        # Move focus
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
 
-        # --- Keyboard Bindings for Moving Windows Between Workspaces ---
+        "$mod SHIFT, H, movewindow, l"
+        "$mod SHIFT, L, movewindow, r"
+        "$mod SHIFT, K, movewindow, u"
+        "$mod SHIFT, J, movewindow, d"
+
+        # Switch workspaces with mod + [0-9]
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move active window to a workspace with mod + SHIFT + [0-9]
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
         "$mod SHIFT, 3, movetoworkspace, 3"
         "$mod SHIFT, 4, movetoworkspace, 4"
         "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        # Scroll through existing workspaces with mod + scroll
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+
+        # next workspace on monitor
+        "CONTROL_ALT, right, workspace, m+1"
+        "CONTROL_ALT, left, workspace, m-1"
+
+        # lock
+        "CONTROL_ALT, L, exec, swaylock"
+
+        # Move/resize windows with mod + LMB/RMB and dragging
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindowpixel"
+
+        "$mod, Print, exec, grim -g \"$(slurp)\""
 
       ];
 
