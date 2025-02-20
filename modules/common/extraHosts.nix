83cca0303b962113cc61920e9bd8118fc7c6ed61
sheva <shevaneo@gmail.com>
2025-02-20 20:27:26 +0200

    wildcard resolve for a specific domain

--- modules/common/extraHosts.nix
+++ modules/common/extraHosts.nix
@@ -20,5 +20,20 @@
 
     # local dev hosts
     127.0.0.1 pitstudentportal.learnworlds.develop
+    127.0.0.1 testex-academy.learnworlds.develop
   '';
+
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      address = "/learnworlds.develop/127.0.0.1"; 
+      listen-address = "127.0.0.1";
+      bind-interfaces = true;
+      server = [ "192.168.1.1" ];
+    };
+  };
+
+  networking = {
+    nameservers = [ "127.0.0.1" ];
+  };
 }
