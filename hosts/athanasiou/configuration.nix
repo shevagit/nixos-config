a4bbb3963cad9ec9a42807ee1be8c3b9a4f7e98d
sheva <shevaneo@gmail.com>
2025-03-20 12:46:31 +0200

    add athanasiou's generated config

---
+++ hosts/athanasiou/configuration.nix
@@ -0,0 +1,158 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/common/bluetooth.nix
+      ../../modules/common/virt-manager.nix
+      ../../modules/hardware/amd.nix
+      ../../modules/common/hyprland.nix
+      ../../modules/common/hyprland-extras.nix
+      ../../modules/common/docker.nix
+      ../../modules/common/extraHosts.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-0d8a6bb3-200c-47a8-abc0-a761768296ea".device = "/dev/disk/by-uuid/0d8a6bb3-200c-47a8-abc0-a761768296ea";
+  
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.hostName = "athanasiou";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager = {
+    enable = true;
+    settings = {
+      connectivity.uri = "http://nmcheck.gnome.org/check_network_status.txt";
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Athens";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "el_GR.UTF-8";
+    LC_IDENTIFICATION = "el_GR.UTF-8";
+    LC_MEASUREMENT = "el_GR.UTF-8";
+    LC_MONETARY = "el_GR.UTF-8";
+    LC_NAME = "el_GR.UTF-8";
+    LC_NUMERIC = "el_GR.UTF-8";
+    LC_PAPER = "el_GR.UTF-8";
+    LC_TELEPHONE = "el_GR.UTF-8";
+    LC_TIME = "el_GR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sheva = {
+    isNormalUser = true;
+    description = "andreas sheva";
+    group = "sheva";
+    extraGroups = [ "networkmanager" "wheel" "sudo" ];
+    shell = pkgs.zsh;
+  };
+
+  users.groups.sheva = {
+    gid = 1000;
+  };
+
+  # zsh is enabled in home manager, but is not recognized if I don't add it here
+  programs.zsh.enable = true;
+
+  # enable dbus and fwupd for firmware updates
+  services.dbus.enable = true;
+  services.fwupd.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   wget
+   docker
+   gnome-tweaks
+   comma
+   git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
