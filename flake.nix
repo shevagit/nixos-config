8bc580dba931c637690b04c1c913329bbbfca514
sheva <shevaneo@gmail.com>
2025-03-20 11:39:48 +0200

    define unstable version in flake and use for athanasiou host

--- flake.nix
+++ flake.nix
@@ -2,35 +2,40 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    # home-manager, used for managing user configuration
+    # Define two different nixpkgs versions
+    nixpkgs-24_11.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-24_11";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs-24_11, nixpkgs-unstable, home-manager, ... }: {
     nixosConfigurations = {
-      # TODO please change the hostname to your own
-      simos = nixpkgs.lib.nixosSystem {
+      simos = nixpkgs-24_11.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/simos/configuration.nix
-
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.sheva = import ./hosts/simos/home.nix;
+          }
+        ];
+      };
 
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+      athanasiou = nixpkgs-unstable.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/athanasiou/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sheva = import ./hosts/athanasiou/home.nix;
           }
         ];
       };
