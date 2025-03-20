7b2bdf5ca37b1fba8c3de5ce6abfb3c6ea695947
sheva <shevaneo@gmail.com>
2025-03-20 13:01:23 +0200

    remove amdgpupro

--- modules/hardware/amd.nix
+++ modules/hardware/amd.nix
@@ -3,12 +3,6 @@
 {
     hardware = {
         enableRedistributableFirmware = true;
-        amdgpupro = {
-            enable = true;
-            extraPackages = with pkgs; [
-                rocm-smi
-            ];
-        };
     };
 
     services.xserver.videoDrivers = [ "amdgpu" ];
