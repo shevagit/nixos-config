a40d34424d81c4a03b0a88826755b4a7a3d0f310
sheva <shevaneo@gmail.com>
2025-03-25 21:15:01 +0200

    override vscode config format options in unstable

--- hosts/athanasiou/home.nix
+++ hosts/athanasiou/home.nix
@@ -7,4 +7,19 @@
   wayland.windowManager.hyprland.settings.monitor = [
     "eDP-1,1920x1080@60,0x0,1"
   ];
+  
+  # Override vscode.extensions
+  programs.vscode.extensions = lib.mkForce null;
+
+  programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
+    bbenoist.nix
+    eamodio.gitlens
+    github.copilot
+    github.copilot-chat
+    golang.go
+    mechatroner.rainbow-csv
+    ms-vscode.makefile-tools
+    hashicorp.terraform
+    4ops.terraform
+  ];
 }
