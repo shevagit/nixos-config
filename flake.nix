790c36db463a816f5b02bd9884f2ee33dc17369c
sheva <shevaneo@gmail.com>
2025-03-25 21:05:53 +0200

    use unstable home-manager in order to match nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,19 +6,24 @@
     nixpkgs-24_11.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
+    home-manager-24_11 = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs-24_11";
     };
+
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
-  outputs = inputs@{ nixpkgs-24_11, nixpkgs-unstable, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs-24_11, nixpkgs-unstable, home-manager-24_11, home-manager-unstable, ... }: {
     nixosConfigurations = {
       simos = nixpkgs-24_11.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/simos/configuration.nix
-          home-manager.nixosModules.home-manager
+          home-manager-24_11.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -31,7 +36,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/athanasiou/configuration.nix
-          home-manager.nixosModules.home-manager
+          home-manager-unstable.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
