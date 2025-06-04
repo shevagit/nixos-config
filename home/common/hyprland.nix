5ce2b4726a567cb36bdbdda3b9a3ed3fc3c01602
sheva <shevaneo@gmail.com>
2025-06-04 12:42:15 +0300

    [hyprlock] add time and date

--- home/common/hyprland.nix
+++ home/common/hyprland.nix
@@ -303,6 +303,38 @@
           fade_on_empty = false;
         }
       ];
+
+      animations = {
+          enabled = true;
+          bezier = "linear, 1, 1, 0, 0";
+      };
+
+      animation = [
+        "fadeIn, 1, 5, linear"
+        "fadeOut, 1, 5, linear"
+        "inputFieldDots, 1, 2, linear"
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME";
+          font_size = 30;
+          font_family = "$font";
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = "cmd[update:60000] date +\"%A, %d %B %Y\"";
+          font_size = 25;
+          font_family = "$font";
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
+        }
+      ];
     };
   };
 }
