e70356ca207e04b0ceeefeafced67dee4afbe0d4
sheva <shevaneo@gmail.com>
2025-07-13 13:18:33 +0300

    [nixos-config] use gitlab's version of pipewire override

--- modules/overrides/pipewire.nix
+++ modules/overrides/pipewire.nix
@@ -1,17 +1,16 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      pipewire = prev.pipewire.overrideAttrs (_: {
-        version = "1.4.2";
-        src = prev.fetchFromGitHub {
-          owner = "PipeWire";
-          repo = "pipewire";
-          rev = "1.4.2";
-          sha256 = "uxTzdvmazLNmWqc1v1LGiq34zV9IT0y1vTGc/+JiEU8=";
-        };
-      });
-    })
-  ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    pipewire = pkgs.pipewire.overrideAttrs (oldAttrs: rec {
+      version = "1.4.2";
+      src = pkgs.fetchFromGitLab {
+        domain = "gitlab.freedesktop.org";
+        owner = "pipewire";
+        repo = "pipewire";
+        rev = version;
+        sha256 = "sha256-uxTzdvmazLNmWqc1v1LGiq34zV9IT0y1vTGc/+JiEU8=";
+      };
+    });
+  };
 }
