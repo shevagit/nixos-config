b283b1e2dc85d66e8339afb20755a0c2c19f81be
sheva <shevaneo@gmail.com>
2025-08-06 13:05:05 +0300

    [modules] add tailscale

---
+++ modules/common/tailscale.nix
@@ -0,0 +1,21 @@
+{
+  # Enable Tailscale service
+  services.tailscale = {
+    enable = true;
+    # optional flags:
+    extraUpFlags = [
+        "--accept-routes"
+    ];
+  };
+  
+  # Optional: Open the firewall for Tailscale
+  networking.firewall = {
+    # Allow Tailscale traffic
+    trustedInterfaces = [ "tailscale0" ];
+    # Allow incoming connections on Tailscale interface
+    allowedUDPPorts = [ 41641 ]; # Tailscale port
+  };
+  
+  # Optional: Enable systemd-resolved for MagicDNS
+  services.resolved.enable = true;
+}
