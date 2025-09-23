ec88bf0bea2fd318f37497373a3e9499e1d773d0
sheva <shevaneo@gmail.com>
2025-09-23 13:06:50 +0300

    [nixos-config] add instructions for yubiauth

--- modules/services/yubi-auth.nix
+++ modules/services/yubi-auth.nix
@@ -3,12 +3,17 @@
   # 1) Enable the U2F PAM module system-wide
   security.pam.u2f = {
     enable = true;
-    cue = true;          # shows "Please touch the device"
     control = "sufficient"; 
-    # "sufficient" = either YubiKey OR password works (safer while testing).
-    # Later, switch to "required" to make the key mandatory.
+    settings = {
+        cue = true;
+    };
   };
 
   # 2) Turn on U2F auth only for sudo (not for login, su, etc.)
   security.pam.services.sudo.u2fAuth = true;
 }
+
+## Run the command to generate the keys file on every machine requiring yubikey auth
+
+# mkdir -p ~/.config/Yubico
+#pamu2fcfg -o pam://$HOSTNAME -i pam://$HOSTNAME > ~/.config/Yubico/u2f_keys
