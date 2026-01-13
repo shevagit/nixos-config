8a4b2ee839d1fa6eb304bc4e0476631b9c299d05
sheva <shevaneo@gmail.com>
2026-01-13 16:15:52 +0200

    feat(docker): add a parameter to daemon

--- modules/common/docker.nix
+++ modules/common/docker.nix
@@ -3,6 +3,9 @@
     enable = true;
     autoPrune.enable = true;
     liveRestore = false;
+    daemon.settings = {
+      "min-api-version" = "1.24";
+      };
   };
 
   users.users.sheva.extraGroups = ["docker"];
