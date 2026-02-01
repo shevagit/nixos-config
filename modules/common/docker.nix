879189fc716c9353f9d712b7e5918a2df99bf301
sheva <shevaneo@gmail.com>
2026-02-01 20:35:32 +0200

    fix(nixos-config): remove docker min api version config

--- modules/common/docker.nix
+++ modules/common/docker.nix
@@ -3,9 +3,6 @@
     enable = true;
     autoPrune.enable = true;
     liveRestore = false;
-    daemon.settings = {
-      "min-api-version" = "1.24";
-      };
   };
 
   users.users.sheva.extraGroups = ["docker"];
