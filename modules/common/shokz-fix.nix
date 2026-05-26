07b4e7933750bafeda5d15e2193f3b4d309b4085
sheva <shevaneo@gmail.com>
2026-05-26 15:36:02 +0300

    feat(nixos-config): shokz fix v2

--- modules/common/shokz-fix.nix
+++ modules/common/shokz-fix.nix
@@ -3,6 +3,6 @@
   # sleep/power key event during pairing, causing a system shutdown.
   # Disabling the HID driver on the dongle keeps audio functional.
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="3511", ATTRS{idProduct}=="2f06", DRIVER=="usbhid", ATTR{authorized}="0"
+    SUBSYSTEM=="usb", ATTR{bInterfaceClass}=="03", ATTRS{idVendor}=="3511", ATTRS{idProduct}=="2f06", ATTR{authorized}="0"
   '';
 }
