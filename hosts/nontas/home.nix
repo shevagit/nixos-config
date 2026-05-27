d6bfa905b183e6fbcaf0f33dc530ca667f35ef1b
sheva <shevaneo@gmail.com>
2026-05-27 11:52:25 +0300

    feat(nixos-config): fix resolution on nontas; send touch output to the embedded monitor only

--- hosts/nontas/home.nix
+++ hosts/nontas/home.nix
@@ -24,11 +24,16 @@
   "DP-2,2560x1440@60,0x0,1"
 
   # embedded screen below
-  "eDP-1,1920x1080@60,0x1440,1"
+  "eDP-1,1920x1200@60,0x1440,1"
 
   # catch-all fallback
   ",preferred,auto,1"
   ];
+
+  wayland.windowManager.hyprland.settings.device = {
+    name = "gtch1563:00-2a94:a89b";
+    output = "eDP-1";
+  };
 
   # host-specific packages
   home.packages = with pkgs; [
